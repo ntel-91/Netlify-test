9af8cd3928070e8f75400d92216778c93cb5854f
ntel-91 <ntelenson@gmail.com>
2020-10-15 16:22:43 -0400

    changed phrase

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          TEST NETLIFY
+          I love my brah
         </p>
         <a
           className="App-link"
